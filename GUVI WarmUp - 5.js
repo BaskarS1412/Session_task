23149303331de61ba9f50353e54fb2a7daf8443d
BaskarS1412 <bhaskars1412@gmail.com>
2024-01-02 08:30:06 +0530

    task commit

---
+++ GUVI WarmUp - 5.js
@@ -0,0 +1,19 @@
+/* Create a function that takes an array and returns the first element. */
+
+function getFirstElement(arr) {
+	///Your code Starts here 
+	
+	let arrFirstElmt = arr[0];
+	return arrFirstElmt;
+	
+	
+	
+	///Your code Ends here
+}
+/*
+Lines For TestCase
+
+getFirstElement([1, 2, 3]);
+getFirstElement([80, 5, 100]);
+getFirstElement([-500, 0, 50]);
+*/
---
+++ GUVI WarmUp - 6.js
@@ -0,0 +1,19 @@
+/* Write a function that converts hours into seconds. */
+
+function hourToSeconds(hours) {
+	///Your code Starts here 
+	
+	let hourToSec = hours * 3600;
+	return hourToSec;
+	
+	
+	
+	///Your code Ends here
+}
+/*
+Lines For TestCase
+
+hourToSeconds(24);
+hourToSeconds(10);
+hourToSeconds(2);
+*/
